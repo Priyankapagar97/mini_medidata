7aa7689c184791396ba063526a7f9d316e61b8a1
Priyankapagar97 <priyankapagar97@gmail.com>
2021-05-05 10:05:59 +0530

    test case for validate age, drug & phase

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,3 +1,6 @@
 class Study < ApplicationRecord
-    validates :name, presence: true
+    validates_presence_of :name
+    validates_numericality_of :age_limit, :greater_than_age => 7
+    validates_presence_of :drug
+    validates_numericality_of :phase, :less_than_phase => 5
 end
--- spec/model/study_spec.rb
+++ spec/model/study_spec.rb
@@ -8,10 +8,25 @@ describe Study, type: :model do
         end
     end
 
-    context 'when study have a name' do
+    context 'when study do not have age_limit greater than 7' do
         it 'is valid' do
-            study = Study.new(name: 'my_study')
-            expect(study.valid?).to eq true
+            study = Study.new(age_limit: 6)
+            expect(study.valid?).to eq false
         end
     end
+
+    context 'when study do not have a drug' do
+        it 'is valid' do
+            study = Study.new(drug: '')
+            expect(study.valid?).to eq false
+        end
+    end
+
+    context 'when study do not have phase less than or equal to 5' do
+        it 'is valid' do
+            study = Study.new(phase: 8)
+            expect(study.valid?).to eq false
+        end
+    end
+
 end
