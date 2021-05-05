b0b3591649538f1c5413a108468e93acdf6c28da
Priyankapagar97 <priyankapagar97@gmail.com>
2021-05-05 13:09:40 +0530

    update test case for validate age, drug & phase

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,6 +1,5 @@
 class Study < ApplicationRecord
-    validates_presence_of :name
+    validates_presence_of :name, :drug
     validates_numericality_of :age_limit, :greater_than_age => 7
-    validates_presence_of :drug
     validates_numericality_of :phase, :less_than_phase => 5
 end
